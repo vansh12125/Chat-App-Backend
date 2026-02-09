8ff24b7deb24d4297423137dc10a1cbf1d9de963
vansh12125 <vanshsahu6752@gmail.com>
2026-02-09 15:03:36 +0530

    jwt changes

--- src/main/java/org/example/chatapp/Security/JwtFilter.java
+++ src/main/java/org/example/chatapp/Security/JwtFilter.java
@@ -5,10 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.chatapp.Utils.JwtUtil;
 import org.jspecify.annotations.NonNull;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Configuration
 public class JwtFilter extends OncePerRequestFilter {
 
     @Override
@@ -18,16 +16,29 @@ public class JwtFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws java.io.IOException, jakarta.servlet.ServletException {
 
+        String path = request.getServletPath();
+
+
+        if (path.startsWith("/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String header = request.getHeader("Authorization");
 
-        if (header != null && header.startsWith("Bearer ")) {
-            String token = header.substring(7);
-            try {
-                JwtUtil.validateAndGetUsername(token);
-            } catch (Exception e) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String token = header.substring(7);
+
+        try {
+            JwtUtil.validateAndGetUsername(token);
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
         }
 
         filterChain.doFilter(request, response);
